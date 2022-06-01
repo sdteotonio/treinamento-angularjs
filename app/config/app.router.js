e0656b042b0bb7c17df8a7ca81ccc72aa07b9e38
Sergio Davi <sdteotonio@outlook.com.br>
2022-05-31 22:32:38 -0300

    Updates

--- app/config/app.router.js
+++ app/config/app.router.js
@@ -2,10 +2,19 @@ angular.module("app").config([
   "$stateProvider",
   "$urlRouterProvider",
   function ($stateProvider, $urlRouterProvider) {
-    $stateProvider.state("home", {
-      url: "/home",
-      templateUrl: "public/home/home.template.html",
-    });
+    $stateProvider
+      .state("home", {
+        url: "/home",
+        templateUrl: "public/home/home.template.html",
+      })
+      .state("details", {
+        url: "/details/:characterId",
+        templateUrl:
+          "public/character-details-container/characterDetailsContainer.html",
+        params: {
+          characterId: null,
+        },
+      });
     $urlRouterProvider.otherwise("/home");
   },
 ]);
--- app/public/home/home.controller.js
+++ app/public/home/home.controller.js
@@ -1,20 +1,56 @@
 angular
   .module("app")
-  .controller("HomeController", ["CharactersService", HomeController]);
+  .controller("HomeController", [
+    "CharactersService",
+    "$state",
+    HomeController,
+  ]);
 
-function HomeController(charactersService) {
+function HomeController(charactersService, $state) {
   const vm = this;
   vm.characters = [];
   vm.searchName = "";
+  vm.offset = 0;
 
-  vm.getCharacters = () => {
+  vm.limits = [5, 10, 20];
+  vm.limit = vm.limits[2];
+  vm.totalItems = 0;
+  vm.search = () => {
+    vm.offset = 0;
+    vm.getCharacters(true);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  vm.getCharacters = (reset) => {
     charactersService
-      .getAllCharacters(vm.searchName)
+      .getAllCharacters(vm.searchName, vm.offset, vm.limit)
       .then((response) => {
-        vm.characters = response.data.data.results;
+        vm.totalItems = response.data.data.total;
+        if (reset) {
+          vm.characters = response.data.data.results;
+        } else {
+          vm.characters = [...vm.characters, ...response.data.data.results];
+        }
       })
       .catch((error) => {
         console.log(error);
       });
+  };
+
+  vm.seeDetails = (character) => {
+    console.log("Informação", character);
+    $state.go("details", { characterId: character.id });
+  };
+
+  vm.switchLimit = (limit) => {
+    vm.limit = limit;
+  };
+
+  vm.seeMore = () => {
+    vm.offset += vm.limit;
+    vm.getCharacters();
   };
 }
